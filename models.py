ce4f8083ba769a19bf09b8bb48d54c133a25fdbe
Bethany Morin <bmorin@industrydive.com>
2017-05-08 13:13:24 -0400

    Make the association table more customizable: add per-cat preference level

--- models.py
+++ models.py
@@ -1,14 +1,20 @@
-from sqlalchemy import Table, Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from __init__ import Base
 
 
-cat_toy_association_table = Table(
-    'cat_toy_association',
-    Base.metadata,
-    Column('cat_id', Integer, ForeignKey('cat.id')),
-    Column('cat_toy_id', Integer, ForeignKey('cat_toy.id'))
-)
+class CatToyAssociation(Base):
+    __tablename__ = 'cat_toy_association'
+    cat_id = Column(Integer, ForeignKey('cat.id'), primary_key=True)
+    cat_toy_id = Column(Integer, ForeignKey('cat_toy.id'), primary_key=True)
+    preference_level = Column(Integer)
+    cat_toy = relationship("CatToy", back_populates="cats")
+    cat = relationship("Cat", back_populates="toys")
+
+    def __repr__(self):
+        return "<CatToyAssociation(cat: %s, toy: %s, preference: %s)>" % (
+            self.cat.name, self.cat_toy.name, self.preference_level
+        )
 
 
 class Cat(Base):
@@ -20,9 +26,8 @@ class Cat(Base):
     coloring = Column(String)
     gender = Column(String)
     toys = relationship(
-        "CatToy",
-        secondary=cat_toy_association_table,
-        back_populates='cats'
+        "CatToyAssociation",
+        back_populates='cat'
     )
 
     def __repr__(self):
@@ -35,9 +40,8 @@ class CatToy(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String)
     cats = relationship(
-        "Cat",
-        secondary=cat_toy_association_table,
-        back_populates='toys'
+        "CatToyAssociation",
+        back_populates='cat_toy'
     )
 
     def __repr__(self):
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from __init__ import Base
 from unittest import TestCase
-from models import Cat, CatToy
+from models import Cat, CatToy, CatToyAssociation
 
 
 class BaseTest(TestCase):
@@ -36,7 +36,7 @@ class TestModels(BaseTest):
         )
 
         self.session.add(dewey)
-
+        self.session.commit()
         cat_toys = [
             'tunnel',
             'owl',
@@ -47,19 +47,44 @@ class TestModels(BaseTest):
             'hair tie',
         ]
 
-        gwens_favorite_toys = ['owl', 'rainbow', 'parrot', 'carrot']
-        deweys_favorite_toys = ['tunnel', 'shaky mouse', 'parrot', 'hair tie']
+        gwens_favorite_toys = {
+            'owl': 1,
+            'rainbow': 1,
+            'parrot': 2,
+            'carrot': 3,
+        }
+        deweys_favorite_toys = {
+            'tunnel': 1,
+            'shaky mouse': 2,
+            'parrot': 3,
+            'hair tie': 1
+        }
 
         for toy in cat_toys:
             new_toy = CatToy(name=toy)
             self.session.add(new_toy)
+            self.session.commit()
             if toy in deweys_favorite_toys:
-                dewey.toys.append(new_toy)
+                ct_assoc = CatToyAssociation(
+                    cat_id=dewey.id,
+                    cat_toy_id=new_toy.id,
+                    preference_level=deweys_favorite_toys[toy],
+                )
+                self.session.add(ct_assoc)
             if toy in gwens_favorite_toys:
-                gwen.toys.append(new_toy)
+                ct_assoc = CatToyAssociation(
+                    cat_id=gwen.id,
+                    cat_toy_id=new_toy.id,
+                    preference_level=gwens_favorite_toys[toy],
+                )
+                self.session.add(ct_assoc)
+
         self.session.commit()
-        gwens_toys = self.session.query(CatToy).filter(CatToy.cats.contains(gwen)).all()
+        gwens_toys_query = self.session.query(CatToyAssociation)
+        gwens_toys_query = gwens_toys_query.filter(CatToyAssociation.cat == gwen)
+        gwens_toys_query = gwens_toys_query.filter(CatToyAssociation.preference_level == 1)
+        gwens_toys_query = gwens_toys_query.all()
 
-        gwens_toys_list = [toy.name for toy in gwens_toys]
-
-        self.assertEqual(set(gwens_toys_list), set(gwens_favorite_toys))
+        for toy_obj in gwens_toys_query:
+            self.assertTrue(toy_obj.cat_toy.name in gwens_favorite_toys)
+            self.assertEqual(toy_obj.preference_level, gwens_favorite_toys[toy_obj.cat_toy.name])
