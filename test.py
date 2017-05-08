72f09707607a4677845d722d1107a2e5f74807e8
Bethany Morin <bmorin@industrydive.com>
2017-05-08 12:26:40 -0400

    Make the test file an actual nosetest Remove database building from init. where does this go?

--- test.py
+++ test.py
@@ -1,59 +1,65 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from __init__ import Base
+from unittest import TestCase
 from models import Cat, CatToy
-from __init__ import Base, engine, session
 
-Base.metadata.create_all(bind=engine)
 
-gwen = Cat(
-    name='Gwen',
-    age=19,
-    gender='f',
-    coloring='tuxedo'
-)
+class BaseTest(TestCase):
+    def setUp(self):
+        self.engine = create_engine('sqlite:///:memory:')
+        Base.metadata.create_all(bind=self.engine)
 
-session.add(gwen)
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
 
-dewey = Cat(
-    name='Dewey',
-    age=5,
-    gender='m',
-    coloring='buff tabby'
-)
+    def tearDown(self):
+        Base.metadata.drop_all(bind=self.engine)
 
-session.add(dewey)
 
-cats = session.query(Cat).all()
-print "You have %d cats" % len(cats)
+class TestModels(BaseTest):
+    def test_cat_toy_relationships(self):
+        gwen = Cat(
+            name='Gwen',
+            age=19,
+            gender='f',
+            coloring='tuxedo'
+        )
 
-for cat in cats:
-    print cat
+        self.session.add(gwen)
 
-cat_toys = [
-    'tunnel',
-    'owl',
-    'shaky mouse',
-    'parrot',
-    'rainbow',
-    'carrot',
-    'hair tie',
-]
+        dewey = Cat(
+            name='Dewey',
+            age=5,
+            gender='m',
+            coloring='buff tabby'
+        )
 
-gwens_favorite_toys = ['owl', 'rainbow', 'parrot', 'carrot']
-deweys_favorite_toys = ['tunnel', 'shaky mouse', 'parrot', 'hair tie']
+        self.session.add(dewey)
 
-for toy in cat_toys:
-    new_toy = CatToy(name=toy)
-    session.add(new_toy)
-    if toy in deweys_favorite_toys:
-        dewey.toys.append(new_toy)
-    if toy in gwens_favorite_toys:
-        gwen.toys.append(new_toy)
+        cat_toys = [
+            'tunnel',
+            'owl',
+            'shaky mouse',
+            'parrot',
+            'rainbow',
+            'carrot',
+            'hair tie',
+        ]
 
-print "\n"
-print "Dewey's favorite toys are: "
-for toy in dewey.toys:
-    print toy
-print "\n"
-print "Gwen's favorite toys are: "
-for toy in gwen.toys:
-    print toy
-print "\n"
+        gwens_favorite_toys = ['owl', 'rainbow', 'parrot', 'carrot']
+        deweys_favorite_toys = ['tunnel', 'shaky mouse', 'parrot', 'hair tie']
+
+        for toy in cat_toys:
+            new_toy = CatToy(name=toy)
+            self.session.add(new_toy)
+            if toy in deweys_favorite_toys:
+                dewey.toys.append(new_toy)
+            if toy in gwens_favorite_toys:
+                gwen.toys.append(new_toy)
+        self.session.commit()
+        gwens_toys = self.session.query(CatToy).filter(CatToy.cats.contains(gwen)).all()
+
+        gwens_toys_list = [toy.name for toy in gwens_toys]
+
+        self.assertEqual(set(gwens_toys_list), set(gwens_favorite_toys))
